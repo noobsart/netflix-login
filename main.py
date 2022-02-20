9d1ad41b78d60e7dc34126556d9060782aea0971
izuku.exe <99591012+noobsart@users.noreply.github.com>
2022-02-20 12:42:04 +0530

    Add files via upload
    These are the files edit em if you want idk tho.

---
+++ main.py
@@ -0,0 +1,78 @@
+age = input("Enter your age: ")
+if int(age)<18:
+     print("Sorry,You are underage get help on whythehellareuonnetflixidiotgowatchpokemon.com for further info")
+if int(age)>17:
+    contact=input("enter phone number: ")
+    verify=input("Verify your number by rewriting it: ")
+    if verify==contact:
+       name = input("Ok,You just made an account enter your name: ")
+gender=input("ok "+ name + " can we know what is your gender write m or f: ")
+if gender=="m":
+    mfeed=input("HI,Mr "+ name + " How are you? So what is your type of genre?We have plenty of them Anime,Marvel,bollywood 💩,hollywood?: ?")
+    if mfeed=="anime":
+      anime=input("Cool,which anime mr " + name +"?We have naruto,demon slayer and death note,What would you like to binge? ")
+      if anime=="naruto":
+          print("Thank you for coming here you will be able to see naruto on your browser.Thank you for joining!")
+      elif anime == "demon slayer":
+          print("Sorry Sir this show's rights are pending so when we get the rights we will definetely catch you up ! ")
+      elif anime == "death note":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if mfeed=="Marvel":
+      marvel=input("Cool,which marvel movie mr " + name +"?We only have plenty of them Spider man:no way home,Avenger:endgame and X-men,What would you like to binge? ")
+      if marvel =="spider man:no way home":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif marvel == "avengers:endgame":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif marvel == "x-men":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if mfeed=="bollywood":
+      bollywood=input("Cool,which bollyywood movie mr " + name +"?We have plenty of them znmd,phir hera pheri,minnal murali ,What would you like to binge? ")
+      if bollywood =="znmd":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif bollywood == "phir hera pheri":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif bollywood== "minnal murali":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if mfeed=="hollywood":
+      hollywood=input("Cool,which hollywood movie mr " + name +"?We have interstellar,joker and Parasite,What would you like to binge? ")
+      if  hollywood=="interstellar":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif hollywood== "Joker":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif hollywood == "parasite":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+
+if gender=="f":
+    ffeed=input("HI,Ms "+ name + " How are you? So what is your type of genre?We have plenty of them Anime,Marvel,bollywood 💩,hollywood?: ?")
+    if ffeed=="anime":
+      anime=input("Cool,which anime ms " + name +"?We have naruto,demon slayer and death note,What would you like to binge? ")
+      if anime=="naruto":
+          print("Thank you for coming here you will be able to see naruto on your browser.Thank you for joining!")
+      elif anime == "demon slayer":
+          print("Sorry mam this show's rights are pending so when we get the rights we will definetely catch you up ! ")
+      elif anime == "death note":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if ffeed=="Marvel":
+      marvel=input("Cool,which marvel movie ms " + name +"?We only have plenty of them Spider man:no way home,Avenger:endgame and X-men,What would you like to binge? ")
+      if marvel =="spider man:no way home":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif marvel == "avengers:endgame":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif marvel == "x-men":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if ffeed=="bollywood":
+      bollywood=input("Cool,which bollyywood movie ms " + name +"?We have plenty of them znmd,phir hera pheri,minnal murali ,What would you like to binge? ")
+      if bollywood =="znmd":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif bollywood == "phir hera pheri":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif bollywood== "minnal murali":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
+    if ffeed=="hollywood":
+      hollywood=input("Cool,which hollywood movie ms " + name +"?We have interstellar,joker and Parasite,What would you like to binge? ")
+      if  hollywood=="interstellar":
+          print("Thank you for coming here you will be able to see spider man:no way home on your browser at netflix.com .Thank you for joining!")
+      elif hollywood== "joker":
+          print("cool choice,one of the most rated films on netflix,you will get in netfix.com added into your queue. ")
+      elif hollywood == "parasite":
+          print("Good choice,SO you can go on your browser search for our website and watch it there and dont worry its added in your queue. ")
